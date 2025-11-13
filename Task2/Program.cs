dc7bb9b6ff5aa32bf2f849392501cde38c12aee3
PlatonovaMaria <platoshamaha@gmail.com>
2025-11-13 14:19:12 +0100

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Sales> salesCollection = new List<Sales>
+            {
+                new Sales("Katya", 2510m, "comment"),
+                new Sales("Katya", 500m, "comment"),
+                new Sales("Katya", 750m, "comment"),
+                new Sales("Katya", 12000m, "comment"),
+                new Sales("Katya", 120m, "comment"),
+
+                new Sales("Alina", 150m, "comment"),
+                new Sales("Alina", 200m, "comment"),
+                new Sales("Alina", 180m, "comment"),
+                new Sales("Alina", 2500m, "comment"),
+                new Sales("Alina", 32000m, "comment"),
+
+                new Sales("Maksim", 3000m, "comment"),
+                new Sales("Maksim", 950m, "comment"),
+                new Sales("Maksim", 1100m, "comment"),
+                new Sales("Maksim", 6700m, "comment"),
+                new Sales("Maksim", 145m, "comment"),
+                new Sales("Maksim", 82m, "comment"),
+
+                new Sales("Alina", 800m, ""),
+                new Sales("Katya", 1300m, "  "),
+                new Sales("Katya", 33m, ""),
+                new Sales("Alina", 120m, null),
+                new Sales("Maksim", 7800m, "   ")
+            };
+
+            bonusAdjust(salesCollection);
+            premiumBonus(salesCollection);
+
+            var sortedSalesCollection = salesCollection.GroupBy(x => x.salesManager);
+
+            foreach (var sale in sortedSalesCollection)
+            {
+                Console.Write($"{sale.Key} - ");
+                Console.WriteLine($"{Decimal.Round(sale.Average(x => x.bonus), 2)}");
+            }
+
+        }
+
+        public static List<Sales> bonusAdjust(List<Sales> collection)
+        {
+            foreach (var sale in collection)
+            {
+                if (string.IsNullOrWhiteSpace(sale.comment))
+                {
+                    sale.bonus = 0.5m;
+                }
+            }
+            return collection;
+        }
+
+        public static List<Sales> premiumBonus(List<Sales> collection)
+        {
+            int counter = collection.Count(x => !string.IsNullOrWhiteSpace(x.comment));
+            if (counter > 15)
+            {
+                collection.ForEach(x => x.bonus += 0.4m);
+            }
+            return collection;
+        }
+    }
+}
---
+++ Task2/Sales.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Sales
+    {
+        public string salesManager {get; private set;}
+        public decimal price { get; private set; }
+        public string comment { get; private set; }
+        public decimal bonus { get; set; }
+
+        public Sales (string _manager, decimal _price, string _comment, decimal _bonus = 1m)
+        {
+            salesManager = _manager;
+            price = _price;
+            comment = _comment;
+            bonus = _bonus;
+        }
+    }
+}
