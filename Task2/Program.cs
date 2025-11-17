62beb1e3a8e5c4347b84bf912b9d2c7362eeb8cb
PlatonovaMaria <platoshamaha@gmail.com>
2025-11-17 13:44:01 +0100

    фикс 2го таска

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -10,64 +10,90 @@ namespace Task2
         {
             List<Sales> salesCollection = new List<Sales>
             {
-                new Sales("Katya", 2510m, "comment"),
-                new Sales("Katya", 500m, "comment"),
-                new Sales("Katya", 750m, "comment"),
-                new Sales("Katya", 12000m, "comment"),
-                new Sales("Katya", 120m, "comment"),
+                new Sales(1, "Katya", 2510m, "comment"),
+                new Sales(2, "Katya", 500m, "comment"),
+                new Sales(3, "Katya", 750m, "comment"),
+                new Sales(4, "Katya", 12000m, "comment"),
+                new Sales(5, "Katya", 120m, "comment"),
 
-                new Sales("Alina", 150m, "comment"),
-                new Sales("Alina", 200m, "comment"),
-                new Sales("Alina", 180m, "comment"),
-                new Sales("Alina", 2500m, "comment"),
-                new Sales("Alina", 32000m, "comment"),
+                new Sales(6, "Alina", 150m, "comment"),
+                new Sales(7, "Alina", 200m, "comment"),
+                new Sales(8, "Alina", 180m, "comment"),
+                new Sales(9, "Alina", 2500m, "comment"),
+                new Sales(10, "Alina", 32000m, "comment"),
 
-                new Sales("Maksim", 3000m, "comment"),
-                new Sales("Maksim", 950m, "comment"),
-                new Sales("Maksim", 1100m, "comment"),
-                new Sales("Maksim", 6700m, "comment"),
-                new Sales("Maksim", 145m, "comment"),
-                new Sales("Maksim", 82m, "comment"),
+                new Sales(11, "Maksim", 3000m, "comment"),
+                new Sales(12, "Maksim", 950m, "comment"),
+                new Sales(13, "Maksim", 1100m, "comment"),
+                new Sales(14, "Maksim", 6700m, "comment"),
+                new Sales(15, "Maksim", 145m, "comment"),
+                new Sales(16, "Maksim", 82m, "comment"),
 
-                new Sales("Alina", 800m, ""),
-                new Sales("Katya", 1300m, "  "),
-                new Sales("Katya", 33m, ""),
-                new Sales("Alina", 120m, null),
-                new Sales("Maksim", 7800m, "   ")
+                new Sales(17, "Alina", 800m, ""),
+                new Sales(18, "Katya", 1300m, "  "),
+                new Sales(19, "Katya", 33m, ""),
+                new Sales(20, "Alina", 120m, null),
+                new Sales(21, "Maksim", 7800m, "   ")
             };
-
-            bonusAdjust(salesCollection);
-            premiumBonus(salesCollection);
-
-            var sortedSalesCollection = salesCollection.GroupBy(x => x.salesManager);
-
-            foreach (var sale in sortedSalesCollection)
+            Dictionary<decimal, decimal> saleBonuses = new Dictionary<decimal, decimal>();
+            for (int i = 0; i < salesCollection.Count; i++)
             {
-                Console.Write($"{sale.Key} - ");
-                Console.WriteLine($"{Decimal.Round(sale.Sum(x => x.bonus))}");
+                saleBonuses.Add(salesCollection[i].saleId, salesCollection[i].price * 0.01m);
             }
+
+            bonusAdjust(salesCollection, saleBonuses);
+            premiumBonus(salesCollection, saleBonuses);
+
+            Dictionary<string, decimal> managerBonus = new Dictionary<string, decimal>();
+            foreach (var sale in salesCollection)
+            {
+                if (!managerBonus.ContainsKey(sale.salesManager))
+                {
+                    managerBonus.Add(sale.salesManager, saleBonuses[sale.saleId]);
+                }
+                else
+                {
+                    managerBonus[sale.salesManager] += saleBonuses[sale.saleId];
+                }
+            }    
+            
+            foreach (var pair in managerBonus)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value}");
+            }
+
+            //foreach (var sale in sortedSalesCollection)
+            //{
+            //    Console.Write($"{sale.Key} - ");
+            //    Console.WriteLine($"{Decimal.Round(sale.Sum(x => x.bonus))}");
+            //}
         }
 
-        public static List<Sales> bonusAdjust(List<Sales> collection)
+        public static Dictionary<decimal, decimal> bonusAdjust(List<Sales> collection, Dictionary<decimal, decimal> bonuses)
         {
             foreach (var sale in collection)
             {
                 if (string.IsNullOrWhiteSpace(sale.comment))
                 {
-                    sale.bonus = sale.price * 0.005m;
+                    bonuses[sale.saleId] = sale.price * 0.005m;
+                    //sale.bonus = sale.price * 0.005m;
                 }
             }
-            return collection;
+            return bonuses;
         }
-        //  a * 0.4 + b * 0.4 = 0.4 * ( a + b )
-        public static List<Sales> premiumBonus(List<Sales> collection)
+
+        public static Dictionary<decimal, decimal> premiumBonus(List<Sales> collection, Dictionary<decimal, decimal> bonuses)
         {
             int counter = collection.Count(x => !string.IsNullOrWhiteSpace(x.comment));
             if (counter > 15)
             {
-                collection.ForEach(x => x.bonus += x.bonus * 0.004m);
+                for(int i = 0; i < collection.Count; i++)
+                {
+                    bonuses[collection[i].saleId] += collection[i].price * 0.004m;
+                }
+                //collection.ForEach(x => x.bonus += x.price * 0.004m);
             }
-            return collection;
+            return bonuses;
         }
     }
 }
--- Task2/Sales.cs
+++ Task2/Sales.cs
@@ -8,17 +8,17 @@ namespace Task2
 {
     public class Sales
     {
+        public int saleId { get; private set; }
         public string salesManager {get; private set;}
         public decimal price { get; private set; }
         public string comment { get; private set; }
-        public decimal bonus { get; set; }
 
-        public Sales (string _manager, decimal _price, string _comment)
+        public Sales (int id, string _manager, decimal _price, string _comment)
         {
+            saleId = id;
             salesManager = _manager;
             price = _price;
             comment = _comment;
-            bonus = _price * 0.01m;
         }
     }
 }
